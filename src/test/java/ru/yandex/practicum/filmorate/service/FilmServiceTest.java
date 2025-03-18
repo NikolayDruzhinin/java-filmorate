755f08825725997f2259aad832e68d8311553271
Nikolay Druzhinin <northernNikolay@yandex.ru>
2025-03-18 22:03:43 +0300

    controllers-films-users/task_realization

---
+++ src/test/java/ru/yandex/practicum/filmorate/service/FilmServiceTest.java
@@ -0,0 +1,80 @@
+package ru.yandex.practicum.filmorate.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FilmServiceTest {
+    private FilmService filmService;
+    private Film film1;
+
+    @BeforeEach
+    public void init() {
+        filmService = new FilmService();
+        film1 = Film.builder()
+                .name("test")
+                .description("description")
+                .releaseDate(LocalDate.now())
+                .duration(120)
+                .build();
+    }
+
+    @Test
+    public void Film_create_should_be_valid() {
+        filmService.create(film1);
+        assertEquals(filmService.get().stream().findFirst().get(), film1);
+    }
+
+    @Test
+    public void Film_update_should_be_valid() {
+        filmService.create(film1);
+        Film film2 = film1.toBuilder()
+                .name("New name")
+                .releaseDate(LocalDate.now().minusYears(1))
+                .description("New description")
+                .duration(1)
+                .build();
+        filmService.update(film2);
+        assertEquals(filmService.get().stream().findFirst().get(), film2);
+    }
+
+    @Test
+    public void Film_update_should_throw_NotFoundException() {
+        assertThrows(NotFoundException.class, () -> filmService.update(film1));
+    }
+
+    @Test
+    public void Film_create_should_throw_ConditionsNotMetException() {
+        film1.setName("");
+        assertThrows(ConditionsNotMetException.class, () -> filmService.create(film1));
+
+        film1.setName(null);
+        assertThrows(ConditionsNotMetException.class, () -> filmService.create(film1));
+
+        String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWSYZ";
+        StringBuilder sb = new StringBuilder();
+        Random rand = new Random();
+        for (int i = 0; i < 200; ++i) {
+            sb.append(ALPHABET.charAt(Math.abs(rand.nextInt()) % ALPHABET.length()));
+        }
+
+        film1.setDescription(sb.toString());
+        assertThrows(ConditionsNotMetException.class, () -> filmService.create(film1));
+
+        film1.setReleaseDate(LocalDate.of(1984, Month.DECEMBER, 28));
+        assertThrows(ConditionsNotMetException.class, () -> filmService.create(film1));
+
+        film1.setDuration(-1);
+        assertThrows(ConditionsNotMetException.class, () -> filmService.create(film1));
+    }
+
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/service/UserServiceTest.java
@@ -0,0 +1,68 @@
+package ru.yandex.practicum.filmorate.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class UserServiceTest {
+    private UserService userService;
+    private User user1;
+
+    @BeforeEach
+    public void init() {
+        userService = new UserService();
+
+        user1 = User.builder()
+                .login("test")
+                .email("test@ru")
+                .name("test")
+                .birthday(LocalDate.now())
+                .build();
+    }
+
+    @Test
+    public void User_create_should_be_valid() {
+        userService.create(user1);
+        assertEquals(userService.get().stream().findFirst().get(), user1);
+    }
+
+    @Test
+    public void User_update_should_be_valid() {
+        userService.create(user1);
+        User user2 = user1.toBuilder()
+                .login("test2")
+                .email("test2@ru")
+                .build();
+        userService.update(user2);
+        assertEquals(userService.get().stream().findFirst().get(), user2);
+    }
+
+    @Test
+    public void User_create_should_throw_ConditionsNotMetException() {
+        user1.setEmail("");
+        assertThrows(ConditionsNotMetException.class, () -> userService.create(user1));
+        user1.setEmail("test.ru");
+        assertThrows(ConditionsNotMetException.class, () -> userService.create(user1));
+        user1.setBirthday(LocalDate.now().plusYears(1));
+        assertThrows(ConditionsNotMetException.class, () -> userService.create(user1));
+    }
+
+    @Test
+    public void User_create_should_throw_NotFoundException() {
+        user1.setLogin("");
+        assertThrows(NotFoundException.class, () -> userService.create(user1));
+    }
+
+    @Test
+    public void User_update_should_throw_NotFoundException() {
+        user1.setId(2);
+        assertThrows(NotFoundException.class, () -> userService.update(user1));
+    }
+}
